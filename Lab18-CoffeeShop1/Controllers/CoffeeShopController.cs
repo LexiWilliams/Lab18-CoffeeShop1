ca668069fe5f96b363ba41e0c6df505fed752c39
Lexi <lexa.shelby@gmail.com>
2019-08-16 17:42:25 -0400

    Lab 18 Coffee Shop 1 first commit

---
+++ Lab18-CoffeeShop1/Controllers/CoffeeShopController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab18_CoffeeShop1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18_CoffeeShop1.Controllers
+{
+    public class CoffeeShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Registration()
+        {
+
+            return View(new RegisterUser());
+        }
+        [HttpPost]
+        public IActionResult Registration(RegisterUser newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Welcome", newUser);
+            }
+            return View(newUser);
+        }
+        public IActionResult Welcome(RegisterUser user)
+        {
+            return View(user);
+        }
+    }
+}
